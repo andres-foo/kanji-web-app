ca662e61585a123a3bb842a81186760cd130bdc4
andres-foo <jvillanelo@gmail.com>
2023-02-04 18:39:08 -0400

    Reorganize list to split grade list into grades and jlpt list into jlpt levels

--- pages/list.php
+++ pages/list.php
@@ -1,53 +1,113 @@
 <?php define('home', true); ?>
 <?php require '../parts/header.php'; ?>
 
-<?php if (isset($_GET['list'])): ?>
+<?php if (!isset($_GET['list'])): ?>
+<div class="list">
+    No list selected.
+</div>
+
+<?php else: ?>
+
 <?php
 // db connection
 $myPDO = new PDO('sqlite:../data/kanjis.db');
 
-if($_GET['list'] == 'jlpt') {
-    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.jlpt IS NOT NULL ORDER BY kanjis.jlpt DESC";
-    $stmt = $myPDO->query($sql);
+function loop_entries($entries) {
+    $text = '';
+    foreach($entries as $entry) {
+        $text .= '<a href="kanji.php?literal='.$entry['literal'].'"';
+        if($entry['added'] == 1) $text .= ' class="added"';
+        $text .= '>'.$entry['literal'].'</a>'; 
+    }
+    return $text;
+}
+
+function text_jlpt($PDO, $level) {
+    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.jlpt = ?";
+    $stmt = $PDO->prepare($sql);
+    $result = $stmt->execute([$level]);
     $entries = $stmt->fetchAll();
-}  elseif($_GET['list'] == 'heisg6') {
-    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.heisg6 IS NOT NULL ORDER BY kanjis.heisg6 ASC";
-    $stmt = $myPDO->query($sql);
+    $text = '<div class="title">JLPT '.$level.' ('.count($entries).' characters):</div>';
+    foreach($entries as $entry) {
+        $text .= '<a href="kanji.php?literal='.$entry['literal'].'"';
+        if($entry['added'] == 1) $text .= ' class="added"';
+        $text .= '>'.$entry['literal'].'</a>'; 
+    }
+    return $text;
+}
+
+function text_grade($PDO, $grade) {
+    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.grade = ?";
+    $stmt = $PDO->prepare($sql);
+    $result = $stmt->execute([$grade]);
     $entries = $stmt->fetchAll();
-}  elseif($_GET['list'] == 'jouyou') {
-    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.grade IS NOT NULL AND kanjis.grade <= 8 ORDER BY kanjis.grade ASC";
-    $stmt = $myPDO->query($sql);
-    $entries = $stmt->fetchAll();
-}  elseif($_GET['list'] == 'frequency') {
-    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.frequency IS NOT NULL ORDER BY kanjis.frequency ASC";
-    $stmt = $myPDO->query($sql);
-    $entries = $stmt->fetchAll();
-}  elseif($_GET['list'] == 'my_list') {
-    $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis_study.added = 1";
-    $stmt = $myPDO->query($sql);
-    $entries = $stmt->fetchAll();
-}  else {
-    exit("No such list.");
+    if($grade != 8) {
+        $text = '<div class="title">PRIMARY SCHOOL - GRADE '.$grade.' ('.count($entries).' characters):</div>';
+    } else {
+        $text = '<div class="title">SECONDARY SCHOOL ('.count($entries).' characters):</div>';
+    }
+    $text .= loop_entries($entries);
+    return $text;
 }
 ?>
 
+
+
 <div class="list">
-    <?php if(!$entries): ?>
-    No entries for this list.
+    <?php if($_GET['list'] == 'jlpt'): ?> 
+        <?php 
+            echo text_jlpt($myPDO, 4);
+            echo text_jlpt($myPDO, 3);
+            echo text_jlpt($myPDO, 2);
+            echo text_jlpt($myPDO, 1);
+        ?>
+    <?php elseif($_GET['list'] == 'jouyou') :?>
+        <?php 
+            echo text_grade($myPDO, 1);
+            echo text_grade($myPDO, 2);
+            echo text_grade($myPDO, 3);
+            echo text_grade($myPDO, 4);
+            echo text_grade($myPDO, 5);
+            echo text_grade($myPDO, 6);
+            echo text_grade($myPDO, 8);
+        ?>     
+       
+    <?php elseif($_GET['list'] == 'my_list'): ?>
+        <?php
+            $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis_study.added = 1";
+            $stmt = $myPDO->query($sql);
+            $entries = $stmt->fetchAll();
+        ?>
+        <div class="title">MY STUDY LIST (<?php echo count($entries); ?> characters):</div>
+        <?php echo loop_entries($entries); ?>
+
+    <?php elseif($_GET['list'] == 'heisg6'): ?>
+        <?php
+            $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.heisg6 IS NOT NULL ORDER BY kanjis.heisg6 ASC";
+            $stmt = $myPDO->query($sql);
+            $entries = $stmt->fetchAll();
+        ?>
+        <div class="title">MY STUDY LIST (<?php echo count($entries); ?> characters):</div>
+        <?php echo loop_entries($entries); ?>
+
+    <?php elseif($_GET['list'] == 'frequency'): ?>
+        <?php
+            $sql = "SELECT kanjis.literal, kanjis_study.added FROM kanjis LEFT JOIN kanjis_study ON kanjis.literal = kanjis_study.literal WHERE kanjis.frequency IS NOT NULL ORDER BY kanjis.frequency ASC";
+            $stmt = $myPDO->query($sql);
+            $entries = $stmt->fetchAll();
+        ?>
+        <div class="title">MY STUDY LIST (<?php echo count($entries); ?> characters):</div>
+        <?php echo loop_entries($entries); ?>
+
+
     <?php else: ?>
-        <div class="title">Showing <?php echo count($entries); ?> characters:</div>
-        <?php foreach($entries as $entry): ?>
-            <a href="kanji.php?literal=<?php echo $entry['literal']; ?>"<?php if($entry['added'] == 1) echo 'class="added"';?>>
-                <?php echo $entry['literal']; ?>
-            </a>
-        <?php endforeach; ?>
-    <?php endif; ?>
+            No such list.
+    <?php endif; //list == ? ?>
+
+
 </div>
 
-<?php else: ?>
-    <div class="list">
-        No list selected.
-    </div>
-<?php endif; ?>
+
+<?php endif; //isset list ?>
                 
 <?php require '../parts/footer.php'; ?>
