5d4886f93069797d021e6cfe79557196b5e1b0a1
andres-foo <jvillanelo@gmail.com>
2025-11-02 21:44:44 -0300

    Removed ai input field from add-phrase after both gemini and chatgpt can't seem to do both steps of creating a ophrase and adding ruby text to it simultaneously in a consisten manner

--- pages/add-phrase.php
+++ pages/add-phrase.php
@@ -9,24 +9,19 @@
 
     <div class="add-example">
         <form action="../actions/add-phrase.php" method="POST">
-            <span>AI input</span>
-            <input type="text" name="ai" placeholder="phrase;phrase_ruby;translation" id="aiInput">
 
-            <span>Phrase</span>
-            <input type="text" name="phrase" placeholder="りんご以外はいりません。" id="phrase">
-            <span>Phrase with ruby</span>
-            <input type="text" name="phrase_ruby" placeholder="りんご<ruby>以外<rt>いがい</rt></ruby>はいりません。" id="phraseRuby">
-            <span>Translation</span>
-            <input type="text" name="translation" placeholder="I don’t need anything except apples." id="translation">
+            <p>Phrase</p>
+            <input type="text" name="phrase" placeholder="りんご以外はいりません。">
+            <p>Phrase with ruby</p>
+            <input type="text" name="phrase_ruby" placeholder="りんご<ruby>以外<rt>いがい</rt></ruby>はいりません。" id="ruby">
+            <div id="preview"></div>
+            <p>Translation</p>
+            <input type="text" name="translation" placeholder="I don’t need anything except apples.">
             <p><button type="submit">Add phrase</button></p>
 
 
         </form>
-        <div class="phrase-preview" id="preview">
-            <span class="phrase-preview-ruby" id="previewRuby"></span>
-            <span class="phrase-preview-normal" id="previewNormal"></span>
-            <span class="phrase-preview-translation" id="previewTranslation"></span>
-        </div>
+
     </div>
 
 </div>
@@ -35,31 +30,10 @@
 
 <script>
     let preview = document.querySelector("#preview");
+    let ruby = document.querySelector("#ruby");
 
-    let aiInput = document.querySelector("#aiInput");
-    let phrase = document.querySelector("#phrase");
-    let phraseRuby = document.querySelector("#phraseRuby");
-    let translation = document.querySelector("#translation");
-
-    let previewRuby = document.querySelector("#previewRuby");
-    let previewNormal = document.querySelector("#previewNormal");
-    let previewTranslation = document.querySelector("#previewTranslation");
-
-    aiInput.onchange = () => {
-        let ai = aiInput.value;
-        let [
-            phrasev,
-            phraseRubyv,
-            translationv
-        ] = ai.split(";");
-
-        phrase.value = phrasev;
-        phraseRuby.value = phraseRubyv;
-        translation.value = translationv;
-
-        preview.style.display = "flex";
-        previewRuby.innerHTML = phraseRubyv;
-        previewNormal.innerHTML = phrasev;
-        previewTranslation.innerHTML = translationv;
+    ruby.onchange = () => {
+        preview.style.display = "inline-block";
+        preview.innerHTML = ruby.value;
     }
 </script>
